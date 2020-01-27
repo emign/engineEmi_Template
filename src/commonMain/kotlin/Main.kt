ef83e20e7b5d3c44201d500c6b9dc77eef089538
Nicolai Emig <nicolai.emig@amseegym.de>
2020-01-28 00:04:23 +0100

    Initial commit

---
+++ src/commonMain/kotlin/Main.kt
@@ -0,0 +1,47 @@
+import me.emig.engineEmi.*
+
+
+/**
+ * Das Default (und eigentlich immer) das einzige Engine-Objekt
+ */
+val engine = Engine()
+
+/**
+ * Startpunkt für alle Programme.
+ * Hier werden u.a. [Ebody] und [CanvasElement] Objekte bei der [Engine] registriert.
+ * Es gibt drei Bereiche:
+ * init : Dieser Code-Block wird zur Konfiguration der Engine verwendet. Hier kann man z.B. die Höhe und Breite des Fensters festlegen.
+ * Wenn man diesen Block leer lässt, werden Standard-Werte geladen
+ * viewWillLoad: Dieser Code-Block wird NACH der Konfiguration aber VOR dem Aufbau des Views (der Anzeige) ausgeführt. Hier sollte man
+ * seine Objekte bei der Engine registrieren
+ * viewDidLoad: Dieser Code-Block wird NACH dem der View komplett aufgebaut wurde ausgeführt. Hier sollte man Code platzieren, der darauf
+ * angewiesen ist, dass Objekte bereits fertig erstellt und registriert wurden. Dies trifft vor allem auf [Ebody] Objekte zu.
+ */
+suspend fun main() {
+        engine.run {
+
+            /**
+             * Code um die Engine zu konfigurieren.
+             */
+            init {
+
+            }
+
+            /**
+             * Code der VOR dem Aufbau des Views ausgeführt wird
+             */
+            viewWillLoad {
+
+            }
+
+            /**
+             * Code, der NACH dem Aufbau des Views ausgeführt wird
+             */
+            viewDidLoad {
+                engine.registerController(CamController(engine.camera))
+            }
+
+            start()
+        }
+}
+
