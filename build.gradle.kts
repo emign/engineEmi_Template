b3dda15bbf6a4d8584dfdf2223d3b3f6891a989f
Nicolai Emig <nicolai.emig@amseegym.de>
2020-03-12 15:13:14 +0100

    0.57 switch to Kotlin DSL Gradle Wrapper 6.2.2

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    val engineEmiVersion = "0.57"
+
+    repositories {
+        maven {
+            url = uri("https://dl.bintray.com/emign/engineEmi")
+        }
+        maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        mavenCentral()
+        mavenLocal()
+    }
+    dependencies {
+        classpath("me.emig:engineEmiGradlePlugin:$engineEmiVersion")
+    }
+}
+
+apply<me.emig.engineEmi.gradle.EngineEmiGradlePlugin>()
