68ffbd4fc0afee2d9a7429abe7b7b96c88ed9fdb
Nicolai Emig <nicolai.emig@amseegym.de>
2020-05-15 10:57:02 +0200

    0.77 sample

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,42 +1,11 @@
 import me.emig.engineEmi.*
+import me.emig.engineEmi.graphics.bilder.*
 
 
-/**
- * Startpunkt für alle Programme.
- * Hier werden u.a. [Ebody] und [CanvasElement] Objekte bei der [Engine] registriert.
- * Es gibt drei Bereiche:
- * init : Dieser Code-Block wird zur Konfiguration der Engine verwendet. Hier kann man z.B. die Höhe und Breite des Fensters festlegen.
- * Wenn man diesen Block leer lässt, werden Standard-Werte geladen
- * viewWillLoad: Dieser Code-Block wird NACH der Konfiguration aber VOR dem Aufbau des Views (der Anzeige) ausgeführt. Hier sollte man
- * seine Objekte bei der Engine registrieren
- * viewDidLoad: Dieser Code-Block wird NACH dem der View komplett aufgebaut wurde ausgeführt. Hier sollte man Code platzieren, der darauf
- * angewiesen ist, dass Objekte bereits fertig erstellt und registriert wurden. Dies trifft vor allem auf [Ebody] Objekte zu.
- */
-suspend fun main() {
-        engine.run {
+val engineConfig = EngineConfig(width = 600, height = 600)
 
-            /**
-             * Code um die Engine zu konfigurieren.
-             */
-            init {
-
-            }
-
-            /**
-             * Code der VOR dem Aufbau des Views ausgeführt wird
-             */
-            viewWillLoad {
-
-            }
-
-            /**
-             * Code, der NACH dem Aufbau des Views ausgeführt wird
-             */
-            viewDidLoad {
-                
-            }
-
-            start()
-        }
+suspend fun main() = Engine(engineConfig) {
+	val hut = Bild(120,120,"hut.png")
+	addChild(hut)
 }
 
